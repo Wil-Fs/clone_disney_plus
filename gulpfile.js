41da2f088bfb1f6b199e753b506c9c94887f3edb
William <williamfs133@gmail.com>
2022-12-14 14:04:56 -0300

    Fix: watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-const { dest } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function styles() {
-    gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/*scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
 exports.default = styles;
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.series(styles));
 }
