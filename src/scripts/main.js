d32a01bd138f10262db05d817c955b7a717244a6
William <williamfs133@gmail.com>
2022-12-27 20:19:40 -0300

    Layout mobile resposive: Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const heroSection = document.querySelector('.hero');
+    const heroHight = heroSection.clientHeight;
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');    
 
+    window.addEventListener('scroll', function(){
+        const scrollPosition = window.scrollY;
+        
+        if(scrollPosition < heroHight) {
+            addClassHidden();
+        } else {
+            removeClassHidden();
+        }
+    })
+
+    //Section of atractions, and tabs;
     for(var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const tabTarget = botao.target.dataset.tabButton;
@@ -13,11 +26,23 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+
+    //Section FAQ and accordion;
     for(var i=0; i < questions.length; i++) {
         questions[i].addEventListener('click', openOrClose);
     }
 
 });
+
+function addClassHidden() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function removeClassHidden() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function openOrClose(element) {
     const classElement = 'faq__questions__item--is-open';
